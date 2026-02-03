b3369907f08adb40a9309e1cf9c52bfb8a90025b
ReZ0iro <rezomoon@gmail.com>
2026-02-03 09:47:17 +0330

    Split These Field haye crated_at , created_by , updated_at ,upddated_by From Person Base Model And created and add AuditModel

--- auth_api/libs/db/models.py
+++ auth_api/libs/db/models.py
@@ -4,3 +4,11 @@ from django.conf import settings
 # # Create Your Abstract Model : 
 
 
+class AuditModel(models.Model) : 
+
+    created_by  = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.SET_NULL , null = True , related_name= "+" , editable=False) 
+    created_at  = models.DateTimeField(auto_now_add=True ,null=True , editable=False)
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL , null = True ,  related_name= "+" , editable=False)  # Va Chon 2ta field dar b 1 model fk mikhore hatman bayad az related_name estefadeh shavad
+    updated_at = models.DateTimeField(auto_now=True , editable=False , null=True ,)
+    class Meta : 
+        abstract = True
