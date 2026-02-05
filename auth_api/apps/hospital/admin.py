7c9bf16485b20a24c85f344896c166b3f2711c8e
ReZ0iro <rezomoon@gmail.com>
2026-02-05 18:14:49 +0330

    Add UserHospitalMembership To Admin Pannel

--- auth_api/apps/hospital/admin.py
+++ auth_api/apps/hospital/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Hospital , Departement
+from .models import Hospital , Departement, UserHospitalMembership
 # Register your models here.
 
 
@@ -13,3 +13,9 @@ class DepartementAdminModel(admin.ModelAdmin) :
     list_display    = ["name", "code", "is_active", "hospital"]
 
 admin.site.register(Departement ,DepartementAdminModel )
+
+
+
+class UserHospitalMembershipAdminModel(admin.ModelAdmin) : 
+    list_display = ["user", "hospital", "departement", "role", "is_active", "joined_at"]
+admin.site.register(UserHospitalMembership , UserHospitalMembershipAdminModel)
