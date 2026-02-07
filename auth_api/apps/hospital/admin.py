f9623caaa73cfdbd1c061dca7779e515f7c918fd
ReZ0iro <rezomoon@gmail.com>
2026-02-07 09:35:43 +0330

    Create Drug and Visit Model Admin Panel

--- auth_api/apps/hospital/admin.py
+++ auth_api/apps/hospital/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Hospital , Departement, UserHospitalMembership
+from .models import Hospital , Departement, UserHospitalMembership , Visit , Drugs
 # Register your models here.
 
 
@@ -19,3 +19,11 @@ admin.site.register(Departement ,DepartementAdminModel )
 class UserHospitalMembershipAdminModel(admin.ModelAdmin) : 
     list_display = ["user", "hospital", "departement", "role", "is_active", "joined_at"]
 admin.site.register(UserHospitalMembership , UserHospitalMembershipAdminModel)
+
+class VisitAdminModel(admin.ModelAdmin) : 
+    list_display    = [ "id", "doctor", "patient", "hospital", "departement", "description", "drugs", "visit_date"]
+admin.site.register(Visit , VisitAdminModel)
+
+class DrugsAdminModel(admin.ModelAdmin) :
+    list_display    = ["id", "name",]
+admin.site.register(Drugs, DrugsAdminModel)
