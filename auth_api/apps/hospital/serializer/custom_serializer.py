6c9bbeedb41aa0345886a962968373489378acb3
Reza <kourosheclash@gmail.com>
2026-02-16 06:20:47 +0330

    First UserHospitalCustomize Serializers !

---
+++ auth_api/apps/hospital/serializer/custom_serializer.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from auth_api.apps.hospital.models import (UserHospitalMembership ,Departement)
+from .base_serializers import DepartementSerializers
+
+# Create Your Custom Serializer : 
+
+class DepartementCustomSerializer(serializers.ModelSerializer) : 
+    class Meta : 
+        model = Departement
+        fields = ("id",
+                  "name",
+                  "code",)
+class UserHospitalMemberShipCustomSerializer(serializers.ModelSerializer) : # Todo  : Should Check With Last V
+    hospital_name       = serializers.CharField(source      = "hospital.name", read_only = True)
+    hospital_id         = serializers.IntegerField(source   = "hospital.id" , read_only = True)
+    hospital_code       = serializers.CharField(source = "hospital.code" , read_only = True)                                                   
+
+    departement         = DepartementCustomSerializer(read_only = True)
+
+    role_id             = serializers.IntegerField(source   = "role.id" , read_only = True)
+    role_name           = serializers.CharField(source   = "role.name" , read_only = True)
+    
+    class Meta : 
+
+        model   = UserHospitalMembership
+
+        fields  = ( "hospital_id",
+                    "hospital_name",
+                    "hospital_code" ,
+                    "departement" ,
+                    "is_active" ,
+                    "role_id" ,
+                    "role_name" ,
+                    
+                    )
