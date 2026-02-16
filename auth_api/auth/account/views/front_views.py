54364c37fe622dc24d819f6c108c92e659a18c65
Reza <kourosheclash@gmail.com>
2026-02-16 15:10:29 +0330

    Fix and clear UserListAPIView class endPoint

--- auth_api/auth/account/views/front_views.py
+++ auth_api/auth/account/views/front_views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView 
 from auth_api.auth.account.serializers.user_serializers import (ProfileSerializers , UpdatePassworddSerializer, UpdateUserSerializer ,BasicUserSerailizer)
 from rest_framework import status
-from auth_api.auth.account.queries.admin_queries import (get_user_hospitals_id ,get_user_list)
+from auth_api.auth.account.queries.admin_queries import (get_user_hospitals_id ,get_user_list_by_hospital_id)
 # Create Your Views : 
 
 
@@ -29,15 +29,21 @@ class UserProfile(APIView) :
         return Response(serializer.data , status=status.HTTP_202_ACCEPTED)
 
 
+
 class UserListAPIView(APIView) : 
-    def get(self ,request) : 
+    def get(self ,request, hospital_id) :
+
+        """
+
+        Get Users List  of The Specified Hospital
+
+        """
+
         user= request.user
         query = get_user_hospitals_id(user)
-        query2 = get_user_list(user , )
+        query2 = get_user_list_by_hospital_id(user , hospital_id)
         serializer = BasicUserSerailizer(query2 ,many = True)
         data = {
-            "data"  : str(query) ,
-            "data2" : str(query2) ,
-            "ser_data" : serializer.data
+            "data" : serializer.data
         }
-        return Response(data=data)
+        return Response(data=data, status=status.HTTP_200_OK)
