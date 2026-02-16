901bdd819891d3028cbb9639986c0ea0bb3676d6
Reza <kourosheclash@gmail.com>
2026-02-16 07:38:57 +0330

    Fix the UserHospitalCustom serailzier and fix the fields

--- auth_api/apps/hospital/serializer/custom_serializer.py
+++ auth_api/apps/hospital/serializer/custom_serializer.py
@@ -10,12 +10,13 @@ class DepartementCustomSerializer(serializers.ModelSerializer) :
         fields = ("id",
                   "name",
                   "code",)
-class UserHospitalMemberShipCustomSerializer(serializers.ModelSerializer) : # Todo  : Should Check With Last V
+class UserHospitalMemberShipCustomSerializer(serializers.ModelSerializer) :
     hospital_name       = serializers.CharField(source      = "hospital.name", read_only = True)
     hospital_id         = serializers.IntegerField(source   = "hospital.id" , read_only = True)
     hospital_code       = serializers.CharField(source = "hospital.code" , read_only = True)                                                   
 
-    departement         = DepartementCustomSerializer(read_only = True)
+    departement_id      = serializers.IntegerField(source   = "departement.id" , read_only = True)
+    departement_name    = serializers.CharField(source =  "departement.name", read_only = True)
 
     role_id             = serializers.IntegerField(source   = "role.id" , read_only = True)
     role_name           = serializers.CharField(source   = "role.name" , read_only = True)
@@ -27,7 +28,8 @@ class UserHospitalMemberShipCustomSerializer(serializers.ModelSerializer) : # To
         fields  = ( "hospital_id",
                     "hospital_name",
                     "hospital_code" ,
-                    "departement" ,
+                    "departement_id" ,
+                    "departement_name",
                     "is_active" ,
                     "role_id" ,
                     "role_name" ,
