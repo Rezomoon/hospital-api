2a452f77cbed49209cc2d49a0ceddb560965ce27
Reza <kourosheclash@gmail.com>
2026-02-17 11:18:17 +0330

    Create New End Point For Show Member Ships Of The Hospitals

--- auth_api/apps/hospital/urls/front_urls.py
+++ auth_api/apps/hospital/urls/front_urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from auth_api.apps.hospital.views import HospitalMemberShipsAPIView
+from auth_api.apps.hospital.views.front_views import HospitalMemberShipsAPIView
 # create Your Urls : 
 
 urlpatterns = [
+
     path("<int:hospital_id>/members/" , HospitalMemberShipsAPIView.as_view())
-    
-    
+
 ]
