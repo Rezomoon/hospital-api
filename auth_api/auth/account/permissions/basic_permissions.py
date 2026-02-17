92e25826a6b980ec6738970593489ee4e0e19f21
Reza <kourosheclash@gmail.com>
2026-02-17 06:17:04 +0330

    Create And Add new Permissions

---
+++ auth_api/auth/account/permissions/basic_permissions.py
@@ -0,0 +1,21 @@
+from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS
+from auth_api.auth.account.queries.admin_queries import ( get_user_hospitals_id , )
+# Create Your Permissions : 
+class NotNurse(permissions.BasePermission) : 
+    def has_permission(self, request, view):
+
+        return bool(request.user and not "Nurse" in request.user.userHospitals.filter(is_active = True).values_list("role__name",flat = True))
+
+class SameHospital(permissions.BasePermission) : 
+    def has_object_permission(self, request, view, obj):
+
+        hospitals_id = get_user_hospitals_id(user=request.user)
+
+        obj_query = obj.userHospitals.filter(is_active = True).values_list("hospital_id" , flat  = True)
+
+        for q in obj_query :
+
+                return bool(q in hospitals_id )
+            
+        return False
