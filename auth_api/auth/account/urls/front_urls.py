ba8c10e00f1d53ff3104bf9c98d74091fdc70667
Reza <kourosheclash@gmail.com>
2026-02-17 06:16:02 +0330

    new url user-details

--- auth_api/auth/account/urls/front_urls.py
+++ auth_api/auth/account/urls/front_urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from auth_api.auth.account.views.front_views import (UserProfile , UserListAPIView)
+from auth_api.auth.account.views.front_views import (UserProfile , UserListAPIView, UserDetailsByIdAPIView)
 
 # Create Your URLS
 urlpatterns = [
     path("profile/" , UserProfile.as_view() ) ,
 
     path("user-list/<int:hospital_id>/" , UserListAPIView.as_view()) ,
+
+    path("user-details/<int:user_id>/" , UserDetailsByIdAPIView.as_view())
     
 ]
