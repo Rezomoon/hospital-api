1aec843a7565e42de8a92ee74cf582394a99f04d
Reza <kourosheclash@gmail.com>
2026-02-17 18:37:15 +0330

    Create AddUser endPoint

--- auth_api/auth/account/urls/front_urls.py
+++ auth_api/auth/account/urls/front_urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from auth_api.auth.account.views.front_views import (UserProfile , UserListAPIView, UserDetailsByIdAPIView, )
+from auth_api.auth.account.views.front_views import (
+    
+    UserProfile ,
+    UserListAPIView,
+    UserDetailsByIdAPIView, 
+                                                        )
+from auth_api.auth.account.views.back_views import (
+     AddUser ,
+
+)
 
 # Create Your URLS
 urlpatterns = [
@@ -8,6 +17,10 @@ urlpatterns = [
     path("user-list/<int:hospital_id>/" , UserListAPIView.as_view()) ,
 
     path("user-details/<int:user_id>/" , UserDetailsByIdAPIView.as_view()) , 
+
+    path("add-user/" , AddUser.as_view()) ,
+    
+
 
     
 ]
