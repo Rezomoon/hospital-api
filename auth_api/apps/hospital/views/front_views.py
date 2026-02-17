a0700b08b68eccbf2930c57898de693ed08f3d36
Reza <kourosheclash@gmail.com>
2026-02-17 11:18:53 +0330

    create CBV For Show member Ships of hospital

---
+++ auth_api/apps/hospital/views/front_views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from auth_api.apps.hospital.queries.hospital_queries import (get_hospital_by_id , get_UserHospitalMembership_by_hospital_id)
+from auth_api.apps.hospital.serializer.base_serializers import UserHospitalMembershipSerializers
+from auth_api.apps.hospital.serializer.custom_serializer import (
+                                                                UserHospitalMemberShipCustomSerializer ,
+                                                                HospitalMemberShipsCustomSerializer)
+from auth_api.auth.account.permissions.basic_permissions import (
+                                                                # IsAdminOrSuperUser , 
+                                                                NotNurse)
+# Create your views here.
+class HospitalMemberShipsAPIView(APIView) : 
+
+    permission_classes = [NotNurse]
+
+    def get(self , request , hospital_id) :
+        user = request.user
+        query = get_UserHospitalMembership_by_hospital_id(user , hospital_id=hospital_id)
+        serializer = HospitalMemberShipsCustomSerializer(query , many = True)
+        data = {
+            "data" : serializer.data
+        }
+        return Response(data , status  = status.HTTP_200_OK)
